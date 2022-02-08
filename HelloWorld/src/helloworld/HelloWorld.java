85ea7ba62a48a4c92b36643f3ba5e9312be52d7f
Walker Ray <work.by.walker@gmail.com>
2022-02-07 19:16:17 -0600

    WR - Maybe now Java Hello World is added

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author workb
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SaySomething();
+    }
+    
+    public static void SaySomething()
+    {
+        System.out.println("Hello World");
+    }
+}
